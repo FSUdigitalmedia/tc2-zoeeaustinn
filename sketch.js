e1260883bebb73a8aa28fab34bfd22b3a3610262
ZOE TALAMANTEZ <zt23f@fsu.edu>
2024-09-03 23:58:11 -0400

    My First Assignment

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
 function setup() {
-  createCanvas(400, 400);
+  // Creates a canvas 600 pixels wide
+  // and 400 pixels high.
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //sky blue background
+  background(82, 23, 130);
+  //sun in top-right corner
+  fill("yellow"); //yellow
+
+  stroke("orange"); //orange outline
+
+  strokeWeight(20); //large outline
+
+  circle(550, 50, 100);
+  //grass on bottom half
+
+  stroke(0);//black outline
+
+  strokeWeight(1);//outline thickness
+
+  fill("green");
+
+  rect(0, 200, 600, 200);
+
+  //emojis
+  textSize(75)
+  text("🪼", mouseX, mouseY) //jellyfish
+  text("🍳", 300, 250) //Fried Egg
 }
